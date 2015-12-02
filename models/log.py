92278edb2bb6b0f4750551ffecdf7673b5ad5c3b
Bardia Heydarinejad <az.bardia13@gmail.com>
2015-12-02 19:58:11 +0330

    Create models

---
+++ models/log.py
@@ -0,0 +1,8 @@
+__author__ = 'bardia'
+
+
+class Log(object):
+    def __init__(self, process, start_time, end_time):
+        self.process = process
+        self.start = start_time
+        self.end = end_time
--- models/process.py
+++ models/process.py
@@ -1,1 +1,10 @@
 __author__ = 'bardia'
+
+
+class Process(object):
+    count = 0
+
+    def __init__(self, arrival_time, execution_time):
+        self.index = Process.count
+        self.arrival = arrival_time
+        self.execution = execution_time
